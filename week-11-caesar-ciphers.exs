968387fc757403a57c6524d67b27dc4e9357378a
gabriele.lana <gabriele.lana@cleancode.it>
2014-10-30 22:20:25 +0100

    week-11: First try to solve it with macros

--- week-11-caesar-ciphers.exs
+++ week-11-caesar-ciphers.exs
@@ -1,12 +1,23 @@
 defmodule CaesarCiphers do
+  @alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-  def encode(<<cp::utf8, rest::binary>>, shift) do
-    <<(cp+shift)::utf8, (encode(rest, shift))::binary>>
+  defmacro new(shift) do
+    quote do
+      cipher_module = Module.concat(CaesarCiphers, "WithShiftOf#{unquote(shift)}")
+      defmodule cipher_module do
+        def encode(<<"A"::utf8, rest::binary>>) do
+          <<"X"::utf8, encode(rest)::binary>>
+        end
+        def encode(<<character::utf8, rest::binary>>) do
+          <<character::utf8, rest::binary>>
+        end
+        def encode(<<>>) do
+          <<>>
+        end
+      end
+      cipher_module
+    end
   end
-  def encode(<<>>, shift) do
-    <<>>
-  end
-
 end
 
 
@@ -14,13 +25,11 @@ ExUnit.start
 
 defmodule CaesarCiphers.Test do
   use ExUnit.Case
+  require CaesarCiphers
 
-  test "encode a string" do
-    assert CaesarCiphers.encode("aaa", 1) == "bbb"
-    assert CaesarCiphers.encode("aaa", 2) == "ccc"
-  end
-
-  test "encode works with codepoints" do
-    assert CaesarCiphers.encode("←", 1) == "↑"
+  test "create a cipher for a particular shift" do
+    cipher = CaesarCiphers.new(3)
+    assert :code.is_loaded(CaesarCiphers.WithShiftOf3) == {:file, :in_memory}
+    assert cipher.encode("A") == "X"
   end
 end
